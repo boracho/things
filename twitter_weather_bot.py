7f332f44826baebc5bc9d620b68292ca01980349
Eric <emstin1@gmail.com>
2015-03-09 01:36:18 -0400

    some changes

--- twitter_weather_bot.py
+++ twitter_weather_bot.py
@@ -1,25 +1,30 @@
 import tweepy, requests, time
-CONSUMER_KEY = 'jwKKVXXsprcTXxAELCbwT2NSi'
-CONSUMER_SECRET = 'XqFMMybRZDJx3LBBY1SI9ejWiAnPW6f6mDZlBZxCLvOOtGevTR'
-ACCESS_KEY = '3073786817-nnXmbkReKpz8mQd1zY2Bot165w4z9hUlwKfNLVs'
-ACCESS_SECRET = '8kfDSBioW9xw0BAtDvIcGPuD5w3j7u3SLVdoije984AUL'
+#some variables for all the little children
+#remember to insert your own api keys for your twitter app
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_KEY = ''
+ACCESS_SECRET = ''
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
-
+#because typing that out is a pain
 newline = '\n'
 
+#it's our url for openweathermap and our payload of url parameters
 url = 'http://api.openweathermap.org/data/2.5/weather?'
+#APPID is the api key for openweathermap.org
 payload = {
             'q': 'Uniontown,pa', 'units': 'imperial',
-            'type': 'like', 'APPID': 'd009cf498679747e03a4350b3f5e761d'
+            'type': 'like', 'APPID': ''
           }
-
+#our main loop.  All kinds of gains.
 while True:
+    #our json response object from owm and some variables obtained therin
     weather = requests.get(url, params=payload).json()
     temp = weather['main']['temp']
     humidity = weather['main']['humidity']
     clouds = weather['clouds']['all']
-    
+    #sending our tweet with our variables and our sleepy-time, so we don't send out 100s of tweets a second or whatever.
     api.update_status(status = 'The current Conditions in Uniontown:\nTepmerature: ' + str(temp) + u'\u00B0F\nHumidity: ' + str(humidity) + '%\nCloud Coverage: ' + str(clouds) + '%')
     time.sleep(3600)
